840a14c4e3f2594e0cea996a0db11efd3b11d02a
Aamir Husain <aamir300@gmail.com>
2018-09-25 00:34:16 -0500

    Test if php loads same page

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require_once('db_config.php');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $sql = "INSERT INTO accounts (account_id, username, password) VALUES (NULL, 'test1', 'test2')";
     $link->query($sql);
-    $sql->close();
     $link->close();
 }
 
@@ -17,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </head>
 
     <body>
-        <form action = '<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>' method = 'post'>
+        <form action = '<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>' method = 'POST'>
             <input type = 'text' name = 'user'>
             <input type = 'text' name = 'pwd'>
             <input type = 'submit' value = 'Submit'>
