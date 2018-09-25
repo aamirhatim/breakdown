1436e8308d138e7305c41bbbde85fb9897cc4e40
Aamir Husain <aamir300@gmail.com>
2018-09-24 23:46:01 -0500

    change db_name

--- db_config.php
+++ db_config.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', 'server183.web-hosting.com');
 define('DB_USERNAME', 'aamixvks');
 define('DB_PASSWORD', '92AWrfiB_UGJw');
-define('DB_NAME', 'accounts');
+define('DB_NAME', 'aamixvks_budget');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
