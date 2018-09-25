79e7bd1ca6c9b0684ea93ec29cedaf412e9bde70
Aamir Husain <aamir300@gmail.com>
2018-09-25 18:55:31 -0500

    Test prints

--- index.php
+++ index.php
@@ -3,23 +3,23 @@ require_once('db_config.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Check if email address already exists
-    $sql = $link->prepare("SELECT email from accounts WHERE email = 'aamir300@gmail.com'");
+    // $sql = $link->prepare("SELECT email FROM accounts WHERE email = 'aamir300@gmail.com'");
     // $sql->bind_param('s', htmlspecialchars($_POST['email']));
-    $result = $link->query($sql);
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo $row;
-        }
-    } else {
-        echo 'No rows!';
-    }
-
-    // $query = "SELECT * from accounts WHERE email =" . trim($_POST['email']);
-    // $result = mysqli_query($link, $query);
-    // while ($row = mysqli_fetch_assoc($result)) {
-    //     echo $row;
+    // $result = $link->query($sql);
+    // if ($result->num_rows > 0) {
+    //     while ($row = $result->fetch_assoc()) {
+    //         echo $row;
+    //     }
+    // } else {
+    //     echo 'No rows!';
     // }
-    // mysqli_close($link);
+
+    $query = "SELECT * FROM accounts WHERE email =\'" . trim($_POST['email'] . "'");
+    $result = mysqli_query($link, $query);
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo $row;
+    }
+    mysqli_close($link);
     echo 'finished';
 
 
