13f83e395964fc643979e440f761d4fe30960f4b
Aamir Husain <aamir300@gmail.com>
2018-09-25 00:55:21 -0500

    Add email info

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 require_once('db_config.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $sql = $link->prepare("INSERT INTO accounts (account_id, username, password) VALUES (NULL, ?, ?)");
-    $sql->bind_param('ss', $user, $pwd);
+    $sql = $link->prepare("INSERT INTO accounts (account_id, email, username, password) VALUES (NULL, ?, ?, ?)");
+    $sql->bind_param('ss', $email, $user, $pwd);
+    $email = $_POST['email'];
     $user = $_POST['user'];
     $pwd = $_POST['pwd'];
     $sql->execute();
@@ -21,6 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     <body>
         <form action = '<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>' method = 'POST'>
+            <input type = 'text' name = 'email'>
             <input type = 'text' name = 'user'>
             <input type = 'text' name = 'pwd'>
             <input type = 'submit' value = 'Submit'>
