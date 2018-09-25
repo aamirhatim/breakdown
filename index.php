e0442d82bd9d154452f046e7e30fcf15c33fceb4
Aamir Husain <aamir300@gmail.com>
2018-09-25 18:13:35 -0500

    Test email check

--- index.php
+++ index.php
@@ -3,17 +3,17 @@ require_once('db_config.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Check if email address already exists
-    // $sql = $link->prepare("SELECT * from accounts WHERE email = ?");
-    // $sql->bind_param('s', $_POST['email']);
-    // $sql->execute();
-    // $result = $sql->get_result();
-    // while($row = $result->fetch_assoc()) {
-    //     echo '<h1>TEST</h1>' . $row[email];
-    //   }
-
-    $sql = $link->prepare("INSERT INTO accounts (account_id, email, username, password) VALUES (NULL, ?, ?, ?)");
-    $sql->bind_param('sss', $_POST['email'], $_POST['user'], $_POST['pwd']);
+    $sql = $link->prepare("SELECT * from accounts WHERE email = ?");
+    $sql->bind_param('s', $_POST['email']);
     $sql->execute();
+    $result = $sql->get_result();
+    while($row = $result->fetch_assoc()) {
+        echo '<h1>TEST</h1>' . $row[email];
+    }
+
+    // $sql = $link->prepare("INSERT INTO accounts (account_id, email, username, password) VALUES (NULL, ?, ?, ?)");
+    // $sql->bind_param('sss', $_POST['email'], $_POST['user'], $_POST['pwd']);
+    // $sql->execute();
     $sql->close();
     $link->close();
 }
