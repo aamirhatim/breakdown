c7dfd23ba82b5c92410c7e11812eb358c51d9770
Aamir Husain <aamir300@gmail.com>
2018-09-25 18:38:25 -0500

    Test email check

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // $sql->fetch();
     // echo $result;
     // $result = $sql->get_result();
-    $query = "SELECT * from accounts WHERE email =" . htmlspecialchars($_POST["email"]);
+    $query = "SELECT * from accounts WHERE email =" . trim($_POST['email']);
     $result = mysqli_query($link, $query);
     while ($row = mysqli_fetch_assoc($result)) {
         echo $row;
