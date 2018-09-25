0d0b4f6b87a0b75523051a422399169563cdb50a
Aamir Husain <aamir300@gmail.com>
2018-09-25 00:15:19 -0500

    Test adding a new user

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
+<?php
+require_once('db_config.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $sql = $link->prepare('INSERT INTO accounts (account_id, username, password) VALUES (NULL, ?, ?)');
+    $sql->bind_param('ss', $user, $pwd);
+    $sql->execute();
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>Sign Up!</title>
+    </head>
 
-  <?php require_once('db_config.php')?>
-
-  <head>
-		<title>Aamir Husain</title>
-  </head>
-
-  <body>
-    <h1>Hello Sub World!</h1>
-  </body>
-
+    <body>
+        <form action = '<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>' method = 'post'>
+            <input type = 'text' name = 'user'>
+            <input type = 'text' name = 'pwd'>
+            <input type = 'submit' value = 'Submit'>
+        </form>
+    </body>
 </html>
