a69484765ab6f85d7decb2b6d8bcf7af2cf5afe0
Aamir Husain <aamir300@gmail.com>
2018-09-26 17:13:27 -0500

    print meta

--- js/link_script.js
+++ js/link_script.js
@@ -26,6 +26,7 @@ $(document).ready(function(){
 
       // Print metadata
       document.getElementById("test").innerHTML = metadata;
+      console.log(JSON.stringify(metadata));
 
       // for (i = 0; i < metadata.length; i++) {
       //   document.getElementById("test").innerHTML += (i+1) + ": " + array[i];
