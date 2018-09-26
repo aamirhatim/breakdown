71ac5d2f80a16d8199b6da60358f1e27e0a6d23f
Aamir Husain <aamir300@gmail.com>
2018-09-26 16:32:19 -0500

    Get access token

--- save_token.php
+++ save_token.php
@@ -8,7 +8,8 @@ require_once('db_config.php');
 // Exchange public token for access_token
 $public_token = htmlspecialchars($_REQUEST['token']);
 $access_token = get_access_token($public_token);
-echo $access_token;
+echo $access_token['access_token'];
+echo $access_token['item_id'];
 
 // Check if token already exists for the client
 // $token_exists = 0;
