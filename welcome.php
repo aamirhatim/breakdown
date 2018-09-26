c5fe610600a4806c597681eafb7c6f09cc166057
Aamir Husain <aamir300@gmail.com>
2018-09-26 13:10:57 -0500

    Test linking bank account

--- welcome.php
+++ welcome.php
@@ -15,10 +15,14 @@ if(!(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)){
 <html>
     <head>
         <title>Welcome!</title>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+        <script src="https://cdn.plaid.com/link/v2/stable/link-initialize.js"></script>
     </head>
 
     <body>
         <h1>YOU'RE LOGGED IN</h1>
         <p><br>Click <a href = 'logout.php'>here</a> to log out.</p>
+
+        <button id="link-button">Link Account</button>
     </body>
 </html>
