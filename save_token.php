f0cbde16e30b4eb5999ee1b4dc024dbdbbdbecbc
Aamir Husain <aamir300@gmail.com>
2018-09-26 15:38:11 -0500

    save token to db

--- save_token.php
+++ save_token.php
@@ -6,7 +6,7 @@ require_once('db_config.php');
 // Check if token already exists for the client
 $token_exists = 0;
 if($sql = $link->prepare("SELECT account_id FROM tokens WHERE access_token = ?")) {
-    mysqli_stmt_bind_param($sql, 's', $acess_token);
+    mysqli_stmt_bind_param($sql, 's', $access_token);
     $access_token = htmlspecialchars($_REQUEST['token']);
     if (mysqli_stmt_execute($sql)) {
         mysqli_stmt_store_result($sql);
@@ -21,11 +21,19 @@ if($sql = $link->prepare("SELECT account_id FROM tokens WHERE access_token = ?")
 
 // If token does not exists, add it to the database
 if (!$token_exists) {
-    $sql = $link->prepare("INSERT INTO tokens (account_id, access_token) VALUES (?, ?)");
-    $sql->bind_param('is', $_SESSION['id'], $access_token);
-    $sql->execute();
-    $sql->close();
-    echo '<h3>New bank account added!</h3>';
+    if($sql = $link->prepare("INSERT INTO tokens (account_id, access_token) VALUES (?, ?)")) {
+        mysqli_stmt_bind_param($sql, 'is', $account_id, $access_token);
+        $account_id = $_REQUEST['id'];
+        if(mysqli_stmt_execute($sql)) {
+            echo '<h3>New bank account added!</h3>';
+        } else {
+            echo mysqli_error($link);
+        }
+    }
+    // $sql->bind_param('is', $_SESSION['id'], $access_token);
+    // $sql->execute();
+    // $sql->close();
+    
 }
 
 // header('location: welcome.php');
