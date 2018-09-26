d776f2931943772871f65aac6c9a261fad3e59d0
Aamir Husain <aamir300@gmail.com>
2018-09-26 15:53:48 -0500

    Read metadata

--- js/link_script.js
+++ js/link_script.js
@@ -17,9 +17,9 @@ $(document).ready(function(){
         // Select Account view is enabled.
         $.post('/get_access_token', {
             public_token: public_token,
-            metadata: metadata,
+            // metadata: metadata,
         });
-        document.write(metedata);
+        document.write(metadata);
         // Run AJAX call to add token to server
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
