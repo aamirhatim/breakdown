e9148e448696c9337e36cc7a8b006a14136cc649
Aamir Husain <aamir300@gmail.com>
2018-09-26 23:17:16 -0500

    debug

--- php/db_service.php
+++ php/db_service.php
@@ -3,12 +3,10 @@
 function get_all_accounts() {
     include('db_config.php');
     if ($sql = $link->prepare('SELECT * FROM tokens WHERE account_id = ?')) {
-        echo 'hello';
         $sql->bind_param('i', $account_id);
-        echo 'hello2';
         $account_id = $_SESSION['id'];
-        echo 'hello3';
         if ($sql->execute()) {
+            echo 'hello';
             $result = $sql->get_result();
             if ($result->num_rows > 0) {
                 echo 'hello!';
