e212132c622630671d436be2a39eace3be304d25
Aamir Husain <aamir300@gmail.com>
2018-09-26 23:15:39 -0500

    debug

--- php/db_service.php
+++ php/db_service.php
@@ -4,20 +4,20 @@ function get_all_accounts() {
     include('db_config.php');
     if ($sql = $link->prepare('SELECT * FROM tokens WHERE account_id = ?')) {
         echo 'hello';
-        // $sql->bind_param('i', $account_id);
-        // $account_id = $_SESSION['id'];
-        // if ($sql->execute()) {
-        //     $result = $sql->get_result();
-        //     if ($result->num_rows > 0) {
-        //         echo 'hello!';
-        //     } else {
-        //         echo 'NO';
-        //         echo mysqli_error($link);
-        //     }
-        // } else {
-        //     echo 'NO no';
-        //     echo mysqli_error($link);
-        // }
+        $sql->bind_param('i', $account_id);
+        $account_id = $_SESSION['id'];
+        if ($sql->execute()) {
+            $result = $sql->get_result();
+            if ($result->num_rows > 0) {
+                echo 'hello!';
+            } else {
+                echo 'NO';
+                echo mysqli_error($link);
+            }
+        } else {
+            echo 'NO no';
+            echo mysqli_error($link);
+        }
     } else {
         echo 'NO NO';
         echo mysqli_error($link);
