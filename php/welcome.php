7747023922c4765b3ababfcc361f945696a24919
Aamir Husain <aamir300@gmail.com>
2018-09-29 15:06:30 -0500

    Prepare the categories attribute for the transactions db

--- php/welcome.php
+++ php/welcome.php
@@ -78,10 +78,9 @@ if(!(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)){
                     echo $t['category'] . ' ' .count($t['category']) . '<br>';
                     $trans_categories = '';
                     for ($i = 0; $i < count($t['category']) - 1; $i++) {
-                        echo $t['category'][$i] . ' ' . '<br>';
-                        // $trans_categories += $t['category'][$i] . ',';
+                        $trans_categories .= $t['category'][$i] . ',';
                     }
-                    $trans_categories += $t['category'][count($t['category']) - 1];
+                    $trans_categories .= $t['category'][count($t['category']) - 1];
                     echo $trans_categories . '<br>';
 
                     $trans_categories = $t['category'];
