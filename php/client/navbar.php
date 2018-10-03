8a7842e6307718fae2b1e437703caa661b164845
Aamir Husain <aamir300@gmail.com>
2018-10-02 22:50:56 -0400

    Clean up code

--- php/client/navbar.php
+++ php/client/navbar.php
@@ -1,6 +1,6 @@
 <div id = 'nav-links'>
   <a class = 'nav-item' href = 'php/welcome.php'>DASHBOARD</a>
   <a class = 'nav-item' href = 'php/client/transactions.php'>TRANSACTIONS</a>
-  <a class = 'nav-item' href = 'php/client/settings.php'>MANAGE</a>
+  <a class = 'nav-item' href = 'php/client/settings.php'>SETTINGS</a>
   <a class = 'nav-item' href = 'php/server/logout_script.php'>LOG OUT</a>
 </div>
