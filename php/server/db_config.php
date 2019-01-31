671b96552ac66365acd118b0935422095f871de8
Aamir Husain <aamir300@gmail.com>
2019-01-31 14:32:42 -0500

    edit database config

--- php/server/db_config.php
+++ php/server/db_config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'server183.web-hosting.com');
-define('DB_USERNAME', 'aamixvks');
-define('DB_PASSWORD', '92AWrfiB_UGJw');
-define('DB_NAME', 'aamixvks_budget');
+define('DB_SERVER', 'server.name');
+define('DB_USERNAME', 'username');
+define('DB_PASSWORD', 'password');
+define('DB_NAME', 'database_name');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
